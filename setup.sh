d8ff8285fdcf089f6ae2942530f9bce8bb8bdccf
Taleh <T4lex999@gmail.com>
2023-03-09 02:14:21 +0400

    setup

--- setup.sh
+++ setup.sh
@@ -12,22 +12,7 @@ cp .env.example .env
 # Generate application key
 ./vendor/bin/sail php artisan key:generate
 
-# Publish Laravel WebSockets migrations
-vendor/bin/sail artisan vendor:publish --provider="BeyondCode\LaravelWebSockets\WebSocketsServiceProvider" --tag="migrations"
 
-# Run database migrations
-./vendor/bin/sail php artisan migrate
-
-
-
-# Publish Laravel WebSockets config
-./vendor/bin/sail artisan vendor:publish --provider="BeyondCode\LaravelWebSockets\WebSocketsServiceProvider" --tag="config"
-
-# Publish Laravel Sail config
-./vendor/bin/sail artisan sail:publish
-
-./vendor/bin/sail php composer dump-autoload
-# Install Node.js dependencies
 ./vendor/bin/sail npm install --no-progress
 
 # Build assets
